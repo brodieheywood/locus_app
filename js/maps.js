4dc6c8f8c4483d0fbd2445532ff0573c99ba173f
hcpark112 <hpark112@my.bcit.ca>
2019-01-02 16:09:08 -0800

    maps

--- js/maps.js
+++ js/maps.js
@@ -1,7 +1,24 @@
 var map;
+
+var userLat;
+var userLon;
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position){
+    userLat = position.coords.latitude;
+    userLon = position.coords.longitude;
+    console.log(userLat);
+  });
+}
+
+// } else {
+//   console.log("Geolocation is not supported by this browser.");
+// }
+
 function initMap() {
+
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 49.283423, lng: -123.11530708},
+    center: {lat: userLat, lng: userLon},
     zoom: 18
   });
   var marker = new google.maps.Marker({
@@ -13,6 +30,21 @@ function initMap() {
 
 var locations;
 
+function markers() {
+  // console.log(locations);
+  // console.log(locations[locations]);
+  // console.log(locations.locations);
+  for (place in locations.locations) {
+    console.log(locations.locations[place].lat);
+    console.log(locations.locations[place].lon);
+    var marker = new google.maps.Marker({
+      position: {lat: locations.locations[place].lat, lng: locations.locations[place].lon},
+      map: map,
+      title: "test"
+    });
+  }
+}
+
 function retrieveLocations() {
   $.ajax({
     url: "/getLocations",
@@ -20,6 +52,7 @@ function retrieveLocations() {
     datatype: "json",
     success: function(data) {
       locations = data;
+      markers();
     },
     error: function(error) {
       console.log(error);
