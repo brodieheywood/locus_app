fdf2d41d5b2477969a5da77f83c918fbf3f841b6
hcpark112 <hpark112@my.bcit.ca>
2019-01-02 17:02:45 -0800

    marker tooltips

--- js/maps.js
+++ js/maps.js
@@ -26,21 +26,32 @@ function initMap() {
     map: map,
     title: 'Current Location'
   });
+  var contentString = '<div><h2>This is your current location</h2></div>';
+
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
 }
 
 var locations;
 
 function markers() {
-  // console.log(locations);
-  // console.log(locations[locations]);
-  // console.log(locations.locations);
   for (place in locations.locations) {
-    console.log(locations.locations[place].lat);
-    console.log(locations.locations[place].lon);
-    var marker = new google.maps.Marker({
+    let marker = new google.maps.Marker({
       position: {lat: locations.locations[place].lat, lng: locations.locations[place].lon},
       map: map,
-      title: "test"
+      title: locations.locations[place].name
+    });
+    let contentString = '<div><h1>' + locations.locations[place].name + '</h1><h3>' + locations.locations[place].address + '</h3></div>';
+
+    let infowindow = new google.maps.InfoWindow({
+      content: contentString
+    });
+    marker.addListener('click', function() {
+      infowindow.open(map, marker);
     });
   }
 }
